f0eca90fe9f704a741fb0efc51b4482a62ebaa3f
Travis <tjohnson01@users.noreply.github.com>
2022-05-16 15:06:23 -0600

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -56,13 +56,3 @@ class MyCounter extends HTMLElement {
 
 customElements.define('hello-world', MyCounter);
 
-
-/*class HelloWorld extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `Hello world.  I'm a web component`;
-  }
-}
-
-//register the new custom element
-customElements.define('hello-world', HelloWorld);
-*/
